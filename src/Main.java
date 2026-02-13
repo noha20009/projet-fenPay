d0e00ef6dbd43569e97ac6afed3e701e4a381fcc
noha20009 <machkourinouhayla@gmail.com>
2026-02-13 16:21:42 +0100

    Replace commission input with fixed value of 2

--- src/Main.java
+++ src/Main.java
@@ -140,14 +140,11 @@ public class Main {
 
                 System.out.println("Montant paiement:");
                 double montant = Double.parseDouble(scanner.nextLine());
-
-                System.out.println("Commission:");
-                double commission = Double.parseDouble(scanner.nextLine());
-
+              
                 System.out.println("Facture payée (true/false):");
                 boolean paye = Boolean.parseBoolean(scanner.nextLine());
 
-                statistiqueService.ajouterPaiement(montant, commission, paye);
+                statistiqueService.ajouterPaiement(montant, 2, paye);
             }
 
             else if (choix.equals("2")) {
