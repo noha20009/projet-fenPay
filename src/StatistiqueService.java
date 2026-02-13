c8a8ad19226bba51322f5735d67fb32a33b80e1e
noha20009 <machkourinouhayla@gmail.com>
2026-02-13 16:06:57 +0100

    Ajout commission automatique

--- src/StatistiqueService.java
+++ src/StatistiqueService.java
@@ -1,6 +1,5 @@
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
+import java.time.LocalDate;
 
 public class StatistiqueService {
 
@@ -10,23 +9,57 @@ public class StatistiqueService {
         this.statistique = new Statistique();
     }
 
-    public void ajouterPaiement(double montant, double commission, boolean factureEstPaye) {
 
-        statistique.calculerTotalPaiements(montant);
-        statistique.calculerTotalCommissions(commission);
+public void ajouterPaiement(double montant, double tauxCommission, boolean paye) {
 
-        if (factureEstPaye) {
-            statistique.setFacturePaye(statistique.getFacturePaye() + 1);
-        } else {
-            statistique.setFactureNonPaye(statistique.getFactureNonPaye() + 1);
+    String sqlPaiement = "INSERT INTO Paiement (montant, date, idFacture) VALUES (?, ?, ?)";
+    String sqlCommission = "INSERT INTO Commission (idPaiement, tauxCommission, montant) VALUES (?, ?, ?)";
+
+    try (Connection conn = DatabaseConnection.getConnection()) {
+
+        conn.setAutoCommit(false);
+
+        // insert Paiement
+        PreparedStatement stmtPaiement = conn.prepareStatement(sqlPaiement, Statement.RETURN_GENERATED_KEYS);
+        stmtPaiement.setDouble(1, montant);
+        stmtPaiement.setDate(2, Date.valueOf(LocalDate.now()));
+        stmtPaiement.setInt(3, 1);
+
+        stmtPaiement.executeUpdate();
+
+        ResultSet rs = stmtPaiement.getGeneratedKeys();
+        int idPaiement = 0;
+        if (rs.next()) {
+            idPaiement = rs.getInt(1);
         }
+
+
+        double montantCommission = montant * 2 / 100;
+
+
+        PreparedStatement stmtCommission = conn.prepareStatement(sqlCommission);
+        stmtCommission.setInt(1, idPaiement);
+        stmtCommission.setDouble(2, 2);
+        stmtCommission.setDouble(3, montantCommission);
+
+        stmtCommission.executeUpdate();
+
+        conn.commit();
+
+        System.out.println("Paiement ajouté avec commission automatique !");
+        System.out.println("Commission calculée: " + montantCommission);
+
+    } catch (SQLException e) {
+        e.printStackTrace();
     }
+}
+
 
     public void afficherRapport() {
         statistique.genererRapport();
     }
 
-    // save dans base donnee!!!!
+
     public void saveStatistique() {
 
         String sql = "INSERT INTO Statistique (totalPaiements, totalCommissions, nombrePaiements, nombreFacturePayee, nombreFactureNonPayee, dateGeneration) VALUES (?, ?, ?, ?, ?, ?)";
